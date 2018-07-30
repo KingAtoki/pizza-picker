8d8678029f6f29e9313c9a69d665e7bbdc0275c1
Chris Atoki <christopher.atoki@yahoo.com>
2018-07-29 20:43:25 -0400

    Started writing tests for Review comp

---
+++ src/__test__/Review.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Review from '../Components/RouteComponents/ReviewComponent/Review';
+import App from '../App';
+
+describe('<Review />', () => {
+    it('should call reset state on submit', () => {
+        const appWrapper = shallow(<App />);
+        const submit = appWrapper.instance().submit;
+        const reviewWrapper = shallow(<Review submit={submit} ingredients={['1','2']}/>);
+        const button = reviewWrapper.findWhere(h2 => h2.hasClass('order_btn'));
+        button.prop('onClick')();
+        expect(appWrapper.state().dough).toEqual('Thin Dough');
+        expect(appWrapper.state().ingredients).toEqual([]);
+    });
+})
